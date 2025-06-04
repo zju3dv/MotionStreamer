0c2f1fc7ce2289ca2d2687b33cb5610c3fae25bd
Lixing Xiao <111164872+Li-xingXiao@users.noreply.github.com>
2025-06-04 20:25:02 +0800

    Add files via upload

---
+++ humanml3d_272/prepare/download_Causal_TAE_ckpt.py
@@ -0,0 +1,14 @@
+import os
+os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+
+from huggingface_hub import snapshot_download
+
+snapshot_download(
+  repo_id="lxxiao/MotionStreamer",
+  repo_type="model",
+  local_dir="./",
+  allow_patterns=["Causal_TAE/*"],
+  local_dir_use_symlinks=False,
+  resume_download=True,
+  max_workers=8
+)
